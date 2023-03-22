55fa7159a77af894ce8698b1cbfff4e63105f426
betussozgen <fatmabetulozgen@hotmail.com>
2023-03-22 15:16:01 +0300

    changes App page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,37 +5,43 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   Text,
   View,
   FlatList,
-  StyleSheet
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 import Products from './Products';
 import products_data from './products_data.json'
+import SearchBox from './SearchBox';
+import Header from './Header';
 
 function App() {
-  return(
-
-    <FlatList
-
-    data ={products_data}
-    renderItem={({item}) => <Products products={item} />}
-
-
-    />
-    
-   
+  //const renderProduct = ({item}: {item: any}) => <Products products={item}/>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <SearchBox />
+      <FlatList
+        //showsVerticalScrollIndicator={true}
+        renderItem={({ item }) => <Products products={item} />}
+        data={products_data}
+        numColumns={2}
+      //keyExtractor={(item)=> item.id.toString()}
+      />
+    </SafeAreaView>
   )
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#eceff1'
+    marginHorizontal: 'auto',
+    backgroundColor: 'white',
+    padding: 3,
   }
 })
 export default App;
