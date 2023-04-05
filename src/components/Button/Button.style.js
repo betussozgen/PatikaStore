0fd3a57570d52d1bd49f965ba830e334c1defbba
betussozgen <fatmabetulozgen@hotmail.com>
2023-04-06 00:50:43 +0300

    add button

---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor: 'purple',
+        margin: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+    text:{
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+})
